cc2cc47df1ba716bc39ff54a6745dea68fd5f568
Duke <duke.raphael.ng@gmail.com>
2020-08-13 14:22:16 +1000

    feat: model from json

--- spec/models/tasks_spec.cr
+++ spec/models/tasks_spec.cr
@@ -1,7 +1,7 @@
 require "../spec_helper"
 
-describe Task do
-  pending "should save and delete bookings" do
+describe Task, focus: true do
+  it "should save and delete bookings" do
     task = Task.new
     task.title = "Do the laundry"
     task.note = "Separate clothes by material and colour"
@@ -24,9 +24,12 @@ describe Task do
     task.delete
   end
 
-  # it "should instantiate a task using JSON" do
-  #   task = Task.from_json(%({"title":"Wash the dishes","note":"Clean the cups without detergent"}))
-  #   task.title.should eq("Wash the dishes")
-  #   task.note.to_json.should eq(%("Clean the cups without detergent"))
-  # end
+  it "should instantiate a task using JSON" do
+    task = Task.new(JSON.parse(%({"title":"Wash the dishes","note":"Clean the cups without detergent"})))
+    task.save
+    task.title.should eq("Wash the dishes")
+    task.note.to_json.should eq(%("Clean the cups without detergent"))
+
+    task.delete
+  end
 end
