47843b7430a0bebce19af1b1126c4b084d2c46d4
Duke <duke.raphael.ng@gmail.com>
2020-08-19 14:48:28 +1000

    refactor: add ? null to env port

--- src/constants.cr
+++ src/constants.cr
@@ -11,7 +11,7 @@ module App
   ENVIRONMENT = ENV["SG_ENV"]? || "development"
 
   # ENV PORT for Heroku
-  DEFAULT_PORT          = (ENV["PORT"] || ENV["SG_SERVER_PORT"]? || 3000).to_i
+  DEFAULT_PORT          = (ENV["PORT"]? || ENV["SG_SERVER_PORT"]? || 3000).to_i
   DEFAULT_HOST          = ENV["SG_SERVER_HOST"]? || "0.0.0.0" # "127.0.0.1"
   DEFAULT_PROCESS_COUNT = (ENV["SG_PROCESS_COUNT"]? || 1).to_i
 
